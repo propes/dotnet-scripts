f80e6f0c0b487090d1d70398fff2b2e32cf6bf99
propes <>
2019-03-26 09:43:52 +0000

    amend web api script

--- dotnet-web-api.sh
+++ dotnet-web-api.sh
@@ -24,7 +24,16 @@ dotnet new webapi -o $SLN_FOLDER/$API_PROJ_NAME
 dotnet new xunit -o $SLN_FOLDER/$TEST_PROJ_NAME
 dotnet sln $SLN_FOLDER add $SLN_FOLDER/$API_PROJ_NAME/$API_PROJ_NAME.csproj
 dotnet sln $SLN_FOLDER add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME.csproj
+
+# Link projects 
+
 dotnet add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME.csproj reference $SLN_FOLDER/$API_PROJ_NAME/$API_PROJ_NAME.csproj
+
+
+# Add packages
+
+dotnet add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME. csproj package moq
+
 
 # Create script to run api from root folder
 
