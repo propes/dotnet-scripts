0da1150fdfc8c031aa7a89f2dc03c888c58bde12
propes <>
2019-04-17 10:33:05 +0100

    improve web api script

--- dotnet-web-api.sh
+++ dotnet-web-api.sh
@@ -12,28 +12,34 @@ APP_NAME=$1
 
 # Variables
 
-SLN_FOLDER=src
+SLN_FOLDER=$APP_NAME/src
+CLASSLIB_PROJ_NAME=$APP_NAME
+CLASSLIB_TEST_PROJ_NAME=$APP_NAME.Tests
 API_PROJ_NAME=$APP_NAME.Api
-TEST_PROJ_NAME=$APP_NAME.Api.Tests
+API_TEST_PROJ_NAME=$APP_NAME.Api.Tests
 
 
 # Set up dotnet solution and projects
 
 dotnet new sln -n $APP_NAME -o $SLN_FOLDER
+dotnet new classlib -o $SLN_FOLDER/$CLASSLIB_PROJ_NAME
+dotnet new xunit -o $SLN_FOLDER/$CLASSLIB_TEST_PROJ_NAME
 dotnet new webapi -o $SLN_FOLDER/$API_PROJ_NAME
-dotnet new xunit -o $SLN_FOLDER/$TEST_PROJ_NAME
+dotnet new xunit -o $SLN_FOLDER/$API_TEST_PROJ_NAME
+dotnet sln $SLN_FOLDER add $SLN_FOLDER/$CLASSLIB_PROJ_NAME/$CLASSLIB_PROJ_NAME.csproj
+dotnet sln $SLN_FOLDER add $SLN_FOLDER/$CLASSLIB_TEST_PROJ_NAME/$CLASSLIB_TEST_PROJ_NAME.csproj
 dotnet sln $SLN_FOLDER add $SLN_FOLDER/$API_PROJ_NAME/$API_PROJ_NAME.csproj
-dotnet sln $SLN_FOLDER add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME.csproj
+dotnet sln $SLN_FOLDER add $SLN_FOLDER/$API_TEST_PROJ_NAME/$API_TEST_PROJ_NAME.csproj
 
 # Link projects 
 
-dotnet add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME.csproj reference $SLN_FOLDER/$API_PROJ_NAME/$API_PROJ_NAME.csproj
-
+dotnet add $SLN_FOLDER/$CLASSLIB_TEST_PROJ_NAME/$CLASSLIB_TEST_PROJ_NAME.csproj reference $SLN_FOLDER/$CLASSLIB_PROJ_NAME/$CLASSLIB_PROJ_NAME.csproj
+dotnet add $SLN_FOLDER/$API_TEST_PROJ_NAME/$API_TEST_PROJ_NAME.csproj reference $SLN_FOLDER/$API_PROJ_NAME/$API_PROJ_NAME.csproj
 
 # Add packages
 
-dotnet add $SLN_FOLDER/$TEST_PROJ_NAME/$TEST_PROJ_NAME.csproj package moq
-
+dotnet add $SLN_FOLDER/$CLASSLIB_TEST_PROJ_NAME/$CLASSLIB_TEST_PROJ_NAME.csproj package moq
+dotnet add $SLN_FOLDER/$API_TEST_PROJ_NAME/$API_TEST_PROJ_NAME.csproj package moq
 
 # Create script to run api from root folder
 
